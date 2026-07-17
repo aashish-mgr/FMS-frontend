fad3623d667316719dbb1e79512934f03896a6a9
aashish-mgr <aashishmagar647@gmail.com>
2026-07-17 09:26:25 +0545

    feat: added authApi

---
+++ src/store/api/authApi.ts
@@ -0,0 +1,23 @@
+import { API} from "./index";
+
+interface loginData {
+    userEmail: string,
+    userPassword: string
+}
+
+class authApi {
+    async login(data: loginData) {
+      return await API.post("/auth/login",data);
+    }
+
+    async logout() {
+        return await API.post("/auth/logout");
+    }
+
+    async refresh() {
+        return await API.post("/auth/refresh")
+    }
+}
+
+export default new authApi();
+
---
+++ src/store/api/index.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import store from '../index';
+import { setAccessToken } from '../slices/authSlice';
+import { useDispatch } from 'react-redux';
+
+const dispatch = useDispatch();
+
+
+const API = axios.create({
+    baseURL: 'http://localhost:3000/api',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    withCredentials: true
+})
+
+API.interceptors.request.use(config => {
+    const accessToken = store.getState().auth.accessToken;
+    if(accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config;
+})
+
+API.interceptors.response.use(
+    (response) => response,
+
+    async (error) => {
+
+        if (error.response?.status === 401) {
+
+            const res = await API.post("/auth/refresh");
+
+            dispatch(setAccessToken(res.data?.accessToken))
+
+            error.config.headers.Authorization =
+                `Bearer ${res.data.accessToken}`;
+
+            return API(error.config);
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export {  API };
